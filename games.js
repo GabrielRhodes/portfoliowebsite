1f20896b7eac7573ec407cba15080979b0911f1f
GabrielRhodes <80109380+GabrielRhodes@users.noreply.github.com>
2021-03-10 04:50:31 -0500

    Create games.js

---
+++ games.js
@@ -0,0 +1,7 @@
+function load() {
+    if (window.innerWidth > 500) {
+        document.getElementById("words").innerHTML = "I've created several independent games on my own as well as with others. The games I've created alone are Box and Blocks as well as Myopic Microcosm. I worked in small teams to create Dungeon Crawler, Planetary Conquest, and The Last Great White. All games have proper crediting on the game site.";
+    } else {
+        document.getElementById("words").innerHTML = "I've made several small games that you can check out below. I have done coding for each of these games. Each game has proper crediting on the game site.";
+    }
+}
